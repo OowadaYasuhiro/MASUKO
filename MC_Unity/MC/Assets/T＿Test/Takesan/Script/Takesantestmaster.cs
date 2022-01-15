fa4e147c80faec227d1f37291a54eb190865d8f4
2PC-35\student <20cge0002ot@edu.sendai-com.ac.jp>
2022-01-15 10:28:51 +0900

    土台作成テスト

---
+++ MC_Unity/MC/Assets/T＿Test/Takesan/Script/Takesantestmaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Takesantestmaster : TakesanTestSingletonMonoBehaviour<Takesantestmaster>
+{
+    protected override bool dontDestroyOnLoad { get { return true; } }
+
+    [SerializeField] public static float audiovolume { get; set; }
+
+    [SerializeField] public static int gamedeta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void detaSave()
+    {
+        PlayerPrefs.SetInt("TestDeta",gamedeta);
+        PlayerPrefs.Save();
+    }
+}
