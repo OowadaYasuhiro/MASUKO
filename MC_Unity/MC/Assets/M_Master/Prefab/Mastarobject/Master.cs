c6befdc17081f811b62a94cb2c7672f75b86dcca
OtomoTakeru <20cge0002ot@edu.sendai-com.ac.jp>
2022-02-20 11:11:21 +0900

    修正、編成のデータ作成

--- MC_Unity/MC/Assets/M_Master/Prefab/Mastarobject/Master.cs
+++ MC_Unity/MC/Assets/M_Master/Prefab/Mastarobject/Master.cs
@@ -10,6 +10,7 @@ public class Master : SingletonMonoBehaviour<Master>
     protected override bool dontDestroyOnLoad { get { return true; } }
 
     public static PlayerDeta playerdeta = new PlayerDeta();
+    public static FormationDeta formationdeta = new FormationDeta();
     
 
     private new void Awake()
@@ -26,10 +27,7 @@ public class Master : SingletonMonoBehaviour<Master>
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+        
     }
 
     
