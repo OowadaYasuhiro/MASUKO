56577243f5f8e69023cad0921f3de5d847cd7699
OtomoTakeru <20cge0002ot@edu.sendai-com.ac.jp>
2022-03-17 22:10:16 +0900

    もうなにしたか忘れた、

--- MC_Unity/MC/Assets/M_Master/Prefab/Mastarobject/SceneDeta/Load_Deta.cs
+++ MC_Unity/MC/Assets/M_Master/Prefab/Mastarobject/SceneDeta/Load_Deta.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Load_Deta : MonoBehaviour
+public class Load_Deta 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- MC_Unity/MC/Assets/M_Master/Prefab/Mastarobject/SceneDeta/StageSelect_Deta.cs
+++ MC_Unity/MC/Assets/M_Master/Prefab/Mastarobject/SceneDeta/StageSelect_Deta.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageSelect_Deta : MonoBehaviour
+public class StageSelect_Deta
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
