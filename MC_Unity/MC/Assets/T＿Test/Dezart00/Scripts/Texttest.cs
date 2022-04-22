289f392d65b70274b0058424be841223eb44173b
DESKTOP-5OIA18N\yuday <bisamya349@gmail.com>
2022-04-23 08:18:26 +0900

    no message

---
+++ MC_Unity/MC/Assets/T＿Test/Dezart00/Scripts/Texttest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Texttest : MonoBehaviour
+{
+    public GameObject TextLvel = null;
+    public GameObject TextTotalExperience = null;
+    public GameObject Item_object = null;
+    public int Item_num = 0;
+    public int TotalExperience = 0;
+    public int Lv_num = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) { //Down
+            TotalExperience += 1;
+            if (1000 == TotalExperience)
+            {
+                Lv_num += 1;
+                TotalExperience -= TotalExperience;
+            }
+        }
+        Text TE_Lv = TextTotalExperience.GetComponent<Text>();
+        TE_Lv.text =TotalExperience + "/1000";
+        Text Lv_text = TextLvel.GetComponent<Text>();
+        Lv_text.text = Lv_num + "";
+
+        Text Item_text = Item_object.GetComponent<Text>();
+        Item_text.text = "×" + Item_num;
+    }
+}
