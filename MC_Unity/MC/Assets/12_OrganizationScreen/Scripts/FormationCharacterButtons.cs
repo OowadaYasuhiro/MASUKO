bc578a6269f32f4c127ea7df1423f9500a45e510
2PC-42\student <20cge0018hy@edu.sendai-com.ac.jp>
2022-05-02 17:37:29 +0900

    no message

---
+++ MC_Unity/MC/Assets/12_OrganizationScreen/Scripts/FormationCharacterButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FormationCharacterButtons : MonoBehaviour
+{
+    public void OnClickCharacter(bool isLeft)
+    {
+        GameObject director = GameObject.Find("UIDirector");
+        OrganizationScreen_Deta.isLeftCharacter = isLeft;
+        director.GetComponent<FormationMain>().ShowSelectWindow();
+    }
+
+    public void OnClickArmor(int posNum)
+    {
+        OrganizationScreen_Deta.armorNum = posNum;
+        SceneManager.LoadScene("RecoveryOfBelongingsScreenScene");
+    }
+}
--- MC_Unity/MC/Assets/13_RecoveryOfBelongingsScreen/Scripts/ArmorNodeGeneretor.cs
+++ MC_Unity/MC/Assets/13_RecoveryOfBelongingsScreen/Scripts/ArmorNodeGeneretor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArmorNodeGeneretor : MonoBehaviour
 {
@@ -13,14 +14,28 @@ public class ArmorNodeGeneretor : MonoBehaviour
 
     private GameObject node;
 
+    private int atrifactAmount;
+
+    private List<GameObject> nodes = new List<GameObject>();
+
+    private Color[] testColors = {Color.red, Color.green,Color.blue,Color.cyan, Color.magenta, Color.yellow};
+
     void Start()
     {
+        //todo: 現在所持している遺物数の取得
+
         for (var i = 0; i < testNodeAmount; i++)
         {
             node = Instantiate(nodePrefab);
             node.transform.SetParent(content);
             node.transform.localScale = new Vector3(1f, 1f, 1f);
+            node.transform.localRotation = Quaternion.identity;
             node.transform.localPosition = Vector3.zero;
+            node.GetComponent<NodeMain>().SetNodeIndex(i);
+
+            node.GetComponent<Image>().color = testColors[i];
+
+            nodes.Add(node);
         }
     }
 }
