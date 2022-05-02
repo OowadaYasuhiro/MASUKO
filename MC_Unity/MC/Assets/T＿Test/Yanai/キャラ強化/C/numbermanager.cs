afa006a155063b40a1c8f6aff7165a9e808c5040
OtomoTakeru <20cge0002ot@edu.sendai-com.ac.jp>
2022-05-02 19:50:49 +0900

    これいけるっけ？まあやってみればいっか

---
+++ MC_Unity/MC/Assets/T＿Test/Yanai/キャラ強化/C/numbermanager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class numbermanager : MonoBehaviour
+{
+    [SerializeField] GameObject sinrai;
+    [SerializeField] GameObject charactername;
+    [SerializeField] GameObject Coin;
+    [SerializeField] GameObject Zanryu_Sinen;
+    [SerializeField] GameObject MagaTamashi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キャラクターネーム
+        Text Charactername = charactername.GetComponent<Text>();
+        switch(Master.playerdeta.NowSelectCharactor) {
+            case "Ghost":
+                Charactername.text = "幽霊";
+                break;
+            case "Zashiki_warashi":
+                Charactername.text = "座敷童";
+                break;
+            case "Gore":
+                Charactername.text = "鬼";
+                break;
+            case "Poltergeist":
+                Charactername.text = "ポルターガイスト";
+                break;
+            case "Daemon":
+                Charactername.text = "デーモン";
+                break;
+    　　}
+        //信頼度
+        Sinrai();
+        /*ここから下の三つはプレハブ化されて使わなくなるかも*/
+        //コイン
+        Text coin = Coin.GetComponent<Text>();
+        coin.text = $"{Master.playerdeta.Coin}";
+        //残留思念
+        Text zanryu_Sinen = Zanryu_Sinen.GetComponent<Text>();
+        zanryu_Sinen.text = $"{Master.playerdeta.Zanryu_Sinen}";
+        //勾玉
+        Text magaTamashi = MagaTamashi.GetComponent<Text>();
+        magaTamashi.text = $"{Master.playerdeta.MagaTamashi}";
+    }
+
+    void Sinrai() {
+        Text Sinrai = sinrai.GetComponent<Text>();
+        switch(Master.playerdeta.NowSelectCharactor) {
+            case "Ghost":
+                if(Master.playerdeta.GhostCredibility < 50) {
+                    Sinrai.text = "0";
+                }else if(Master.playerdeta.GhostCredibility < 150) {
+                    Sinrai.text = "1";
+                } else if(Master.playerdeta.GhostCredibility < 250) {
+                    Sinrai.text = "2";
+                }else if(Master.playerdeta.GhostCredibility < 450) {
+                    Sinrai.text = "3";
+                } else {
+                    Sinrai.text = "4";
+                }
+                break;
+            case "Zashiki_warashi":
+                Sinrai.text = $"{Master.playerdeta.Zashiki_warashiCredibility}";
+                break;
+            case "Gore":
+                Sinrai.text = $"{Master.playerdeta.goreCredibility}";
+                break;
+            case "Poltergeist":
+                Sinrai.text = $"{Master.playerdeta.PoltergeistCredibility}";
+                break;
+            case "Daemon":
+                Sinrai.text = $"{Master.playerdeta.DaemonCredibility}";
+                break;
+        }
+    }
+}
---
+++ MC_Unity/MC/Assets/T＿Test/Yanai/キャラ強化/限界突破/overlevelup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class overlevelup : MonoBehaviour
+{
+    [SerializeField] Text level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (Master.playerdeta.NowSelectCharactor)
+        {
+            case "Ghost":
+                level.text = $"{Master.playerdeta.GhostLevel}/50";
+                break;
+            case "Zashiki_warashi":
+                level.text = $"{Master.playerdeta.Zashiki_warashiLevel}/50";
+                break;
+            case "Gore":
+                level.text = $"{Master.playerdeta.GoreLevel}/50";
+                break;
+            case "Poltergeist":
+                level.text = $"{Master.playerdeta.PoltergeistLevel}/50";
+                break;
+            case "Daemon":
+                level.text = $"{Master.playerdeta.DaemonLevel}/50"; 
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
