acfdc058c6761693f4da63df2ba076e6e40f0b20
2PC-42\student <20cge0018hy@edu.sendai-com.ac.jp>
2022-05-23 18:04:58 +0900

    no message

--- MC_Unity/MC/Assets/10_Home/Scripts/HomeMain.cs
+++ MC_Unity/MC/Assets/10_Home/Scripts/HomeMain.cs
@@ -3,25 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HomeMain : MonoBehaviour
+namespace HomeScene
 {
-    /// <summary>
-    /// プレイヤーのデータを取得し反映する
-    /// </summary>
-
-
-    [SerializeField]
-    private GameObject playerLevelText; //プレイヤーレベル
-    [SerializeField]
-    private GameObject playerNameText; //プレイヤー名
-    [SerializeField]
-    private GameObject playerExpGauge; //プレイヤーゲージ
-
-    void Start()
+    public class HomeMain : MonoBehaviour
     {
-        //データを取得
-        playerLevelText.GetComponent<Text>().text = Master.playerdeta.PlayerLevel.ToString();
-        playerNameText.GetComponent<Text>().text = Master.playerdeta.PlayerName;
-        playerExpGauge.GetComponent<Image>().fillAmount = Master.playerdeta.PlayerExperience;
+        /// <summary>
+        /// プレイヤーのデータを取得し反映する
+        /// </summary>
+
+        [SerializeField]
+        private GameObject playerLevelText; //プレイヤーレベル
+        [SerializeField]
+        private GameObject playerNameText; //プレイヤー名
+        [SerializeField]
+        private GameObject playerExpGauge; //プレイヤーゲージ
+        [SerializeField]
+        private GameObject messageWindow;
+        [SerializeField]
+        private Sprite[] characterImages;
+
+        private void Start()
+        {
+            InitializePlayerData();
+
+            messageWindow.SetActive(false);
+        }
+
+        private void InitializePlayerData()
+        {
+            //データを取得
+            playerLevelText.GetComponent<Text>().text = Master.playerdeta.PlayerLevel.ToString();
+            playerNameText.GetComponent<Text>().text = Master.playerdeta.PlayerName;
+            playerExpGauge.GetComponent<Image>().fillAmount = Master.playerdeta.PlayerExperience;
+        }
+
+        private void ReferenceCharacterData()
+        {
+            //現在編成しているキャラクターの名前をセット
+            messageWindow.transform.GetChild(1).GetComponent<Text>().text = Master.playerdeta.NowSelectCharactor;
+
+            //todo: キャラクターデータの設定(セリフとスプライト)
+            //編成しているキャラクターに応じてセリフと画像をセット
+            switch (Master.playerdeta.NowSelectCharactor)
+            {
+                case "Ghost": //幽霊
+                    this.gameObject.GetComponent<Image>().sprite = characterImages[0];
+                    messageWindow.transform.GetChild(2).GetComponent<Text>().text = "Ghost";
+                    break;
+                case "Zasikiwarasi": //座敷童
+                    this.gameObject.GetComponent<Image>().sprite = characterImages[1];
+                    messageWindow.transform.GetChild(2).GetComponent<Text>().text = "Zasikiwarasi";
+                    break;
+                case "Oni": //鬼
+                    this.gameObject.GetComponent<Image>().sprite = characterImages[2];
+                    messageWindow.transform.GetChild(2).GetComponent<Text>().text = "Zasikiwarasi";
+                    break;
+                case "Demon": //デーモン
+                    this.gameObject.GetComponent<Image>().sprite = characterImages[3];
+                    messageWindow.transform.GetChild(2).GetComponent<Text>().text = "Zasikiwarasi";
+                    break;
+                case "Poltergeist": //ポルターガイスト
+                    this.gameObject.GetComponent<Image>().sprite = characterImages[4];
+                    messageWindow.transform.GetChild(2).GetComponent<Text>().text = "Zasikiwarasi";
+                    break;
+                default:
+                    Debug.LogError("error");
+                    break;
+            }
+        }
+
+        private IEnumerator ShowMessageWindow()
+        {
+            messageWindow.SetActive(true);
+            yield return new WaitForSeconds(3.0f);
+            messageWindow.SetActive(false);
+            yield break;
+        }
+
+        public void ShowMessage()
+        {
+            StartCoroutine("ShowMessageWindow");
+        }
     }
 }
