798883a63f9ca90a23638a6cbc6420a31febf22f
2PC-35\student <20cge0002ot@edu.sendai-com.ac.jp>
2022-05-23 17:04:55 +0900

    先にやる事思い出した

--- MC_Unity/MC/Assets/80_MainGame/Script/CharctorModel/MainGameCharactorModel.cs
+++ MC_Unity/MC/Assets/80_MainGame/Script/CharctorModel/MainGameCharactorModel.cs
@@ -4,9 +4,61 @@ using UnityEngine;
 
 public class MainGameCharactorModel : MainGameCharactorState
 {
+    //名前
+    protected string name;
     //向き
     protected bool directionRight;
     //座標
     protected Vector2 position;
+    //移動用
+    protected Vector2[] targetPosition;
+    protected Vector2 latePosition;
+    Vector2 arrow;
+    int moveTargetPointer = 0;
+    bool next = true;
+    //移動速度
+    protected float moveSpeed;
+    //攻撃範囲
+    protected Vector2[] attackRange;
+    //視界範囲
+    protected Vector2[] viewRange;
+    //接敵
+    protected bool findEnemy;
 
+    //移動
+    protected void Move(float timeLine)
+    {
+        if (next == true)
+        {
+            arrow = targetPosition[moveTargetPointer] - position;
+            next = false;
+        }
+        if (next == false)
+        {
+            position += arrow * timeLine * moveSpeed;
+            Vector2 compareArrow = targetPosition[moveTargetPointer] - position;
+            if (!(arrow.Equals(compareArrow.normalized)) || position == targetPosition[moveTargetPointer])
+            {
+                position = targetPosition[moveTargetPointer];
+                moveTargetPointer++;
+                next = true;
+            }
+        }
+    }
+
+    //ノックバック
+    protected void Knockback(bool[,] map)
+    {
+        if (map[(int)(targetPosition[moveTargetPointer - 1] + (-arrow)).x , (int)(targetPosition[moveTargetPointer - 1] + (-arrow)).y] == true)
+        {
+            position = position + (-arrow);
+            moveTargetPointer--;
+            targetPosition[moveTargetPointer] = targetPosition[moveTargetPointer + 1] + (-arrow);
+        }
+        else
+        {
+            position = targetPosition[moveTargetPointer - 1];
+            next = false;
+        }
+    }
 }
