be10d293b47eb45f4756f63a17ea7dd6da414122
2PC-35\student <20cge0002ot@edu.sendai-com.ac.jp>
2022-05-28 17:14:48 +0900

    一旦上げておきたい

--- MC_Unity/MC/Assets/80_MainGame/Script/Event/MainGameSkillEvent.cs
+++ MC_Unity/MC/Assets/80_MainGame/Script/Event/MainGameSkillEvent.cs
@@ -6,7 +6,24 @@ public delegate void SkillEvent();
 
 public class MainGameSkillEvent : MainGameCharactorModel
 {
-    
+    //何フレーム実行するか(0で1回)、何フレーム毎に実行するか(1で毎フレーム)、
+    public IEnumerator DoSkillEvent(Enemy target, int time, int doTick, SkillEvent skillEvent)
+    {
+        int passedTime = 0;
+        POINT:
 
+        if (passedTime >= doTick)
+        {
+            target.AddSkillEvent(skillEvent);
+            passedTime = 0;
+        }
+        time--;
+        passedTime++;
 
+        yield return new WaitForEndOfFrame();
+        if (time >= 0)
+        {
+            goto POINT;
+        }
+    }
 }
