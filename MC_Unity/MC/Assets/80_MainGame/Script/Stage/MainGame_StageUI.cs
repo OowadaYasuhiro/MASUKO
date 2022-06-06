059e7a2c92ce958d7d1918359144abbc01785962
OtomoTakeru <20cge0002ot@edu.sendai-com.ac.jp>
2022-06-06 09:52:14 +0900

    ステージ製作中

---
+++ MC_Unity/MC/Assets/80_MainGame/Script/Stage/MainGame_StageUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame_StageUI : MonoBehaviour
+{
+    //背景
+    [SerializeField] GameObject backGround;
+    //ステージ
+    [SerializeField] GameObject stage;
+}
