f2af282019bd5ca1ac22d77fda1a5fdd28ec0f63
2PC-43\student <20cge0020ys@edu.sendai-com.ac.jp>
2022-06-18 16:04:37 +0900

    タイトルシーン

---
+++ MC_Unity/MC/Assets/T＿Test/Yanai/タイトル/TapStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapStart : MonoBehaviour
+{
+    [SerializeField] Animator Animation = null; 
+    // Start is called before the first frame update
+   public void OnClick() {
+        Debug.Log("タップされた");
+        Animation.SetTrigger("Click");
+        Invoke("Seni",1f);
+    }
+    void Seni() {
+        //ここにホーム画面の名前を入れる。
+        /*Load_Deta.Nextscenename = "";
+        SceneManager.LoadScene("Yanai_TestScene");*/
+    }
+
+}
