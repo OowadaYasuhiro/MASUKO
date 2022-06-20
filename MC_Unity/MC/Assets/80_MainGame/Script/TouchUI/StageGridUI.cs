f8d56436b5b1390d970d9c49b081df509d9d36f5
2PC-35\student <20cge0002ot@edu.sendai-com.ac.jp>
2022-06-20 18:10:24 +0900

    グリッド判定

--- MC_Unity/MC/Assets/80_MainGame/Script/TouchUI/StageGridUI.cs
+++ MC_Unity/MC/Assets/80_MainGame/Script/TouchUI/StageGridUI.cs
@@ -24,13 +24,33 @@ public class StageGridUI : MonoBehaviour
     const float depth6 = -215;
 
     //グリッドの縦線
-    
+    QuadraticIndex quadraticIndex_left0 = new QuadraticIndex(-0.1583f, -816f);
+    QuadraticIndex quadraticIndex_left1 = new QuadraticIndex(-0.1266f, -707.23f);
+    QuadraticIndex quadraticIndex_left2 = new QuadraticIndex(-0.095f, -489.575f);
+    QuadraticIndex quadraticIndex_left3 = new QuadraticIndex(-0.063f, -326.384f);
+    QuadraticIndex quadraticIndex_left4 = new QuadraticIndex(-0.0316f, -163.206f);
+    QuadraticIndex quadraticIndex_left5 = new QuadraticIndex(0, 0);
+    QuadraticIndex quadraticIndex_left6 = new QuadraticIndex(0.0316f, 163.206f);
+    QuadraticIndex quadraticIndex_left7 = new QuadraticIndex(0.063f, 326.384f);
+    QuadraticIndex quadraticIndex_left8 = new QuadraticIndex(0.095f, 489.575f);
+    QuadraticIndex quadraticIndex_left9 = new QuadraticIndex(0.1266f, 707.23f);
+    QuadraticIndex quadraticIndex_left10 = new QuadraticIndex(0.1583f, 816f);
 
     Vector2 cursorPosition;
+    //[SerializeField] GameObject Camera;
+    //[SerializeField] GameObject Image;
+
     private void Start()
     {
-        cursorPosition = GameObject.Find("UICamera").GetComponent<TouchUI>().InputPosition; 
+        //cursorPosition = GameObject.Find("UICamera").GetComponent<TouchUI>().InputPosition; 
+        //cursorPosition = new Vector2(Image.transform.position.x, Image.transform.position.y);
         selectGrid = new Vector2Int(-1,-1);
+    }
+
+    private void Update()
+    {
+        cursorPosition = GameObject.Find("UICamera").GetComponent<TouchUI>().InputPosition;
+        CursorGrid();
     }
 
     void CursorGrid()
@@ -38,31 +58,329 @@ public class StageGridUI : MonoBehaviour
         if (cursorPosition.y > depth0)
         {
             //範囲外
+            selectGrid.x = -1;
+            selectGrid.y = -1;
         }
         else if (cursorPosition.y > depth1)
         {
             selectGrid.y = 5;
-            //if (cursorPosition.x > )
+            if (cursorPosition.x < quadraticIndex_left0.BoundaryCalculation(cursorPosition))
+            {
+                //範囲外
+                selectGrid.x = -1;
+                selectGrid.y = -1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left1.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 0;
+            }
+            else if (cursorPosition.x < quadraticIndex_left2.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left3.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 2;
+            }
+            else if (cursorPosition.x < quadraticIndex_left4.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 3;
+            }
+            else if (cursorPosition.x < quadraticIndex_left5.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 4;
+            }
+            else if (cursorPosition.x < quadraticIndex_left6.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 5;
+            }
+            else if (cursorPosition.x < quadraticIndex_left7.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 6;
+            }
+            else if (cursorPosition.x < quadraticIndex_left8.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 7;
+            }
+            else if (cursorPosition.x < quadraticIndex_left9.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 8;
+            }
+            else if (cursorPosition.x < quadraticIndex_left10.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 9;
+            }
         }
         else if (cursorPosition.y > depth2)
         {
             selectGrid.y = 4;
+            if (cursorPosition.x < quadraticIndex_left0.BoundaryCalculation(cursorPosition))
+            {
+                //範囲外
+                selectGrid.x = -1;
+                selectGrid.y = -1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left1.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 0;
+            }
+            else if (cursorPosition.x < quadraticIndex_left2.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left3.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 2;
+            }
+            else if (cursorPosition.x < quadraticIndex_left4.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 3;
+            }
+            else if (cursorPosition.x < quadraticIndex_left5.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 4;
+            }
+            else if (cursorPosition.x < quadraticIndex_left6.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 5;
+            }
+            else if (cursorPosition.x < quadraticIndex_left7.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 6;
+            }
+            else if (cursorPosition.x < quadraticIndex_left8.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 7;
+            }
+            else if (cursorPosition.x < quadraticIndex_left9.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 8;
+            }
+            else if (cursorPosition.x < quadraticIndex_left10.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 9;
+            }
         }
         else if (cursorPosition.y > depth3)
         {
             selectGrid.y = 3;
+            if (cursorPosition.x < quadraticIndex_left0.BoundaryCalculation(cursorPosition))
+            {
+                //範囲外
+                selectGrid.x = -1;
+                selectGrid.y = -1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left1.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 0;
+            }
+            else if (cursorPosition.x < quadraticIndex_left2.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left3.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 2;
+            }
+            else if (cursorPosition.x < quadraticIndex_left4.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 3;
+            }
+            else if (cursorPosition.x < quadraticIndex_left5.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 4;
+            }
+            else if (cursorPosition.x < quadraticIndex_left6.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 5;
+            }
+            else if (cursorPosition.x < quadraticIndex_left7.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 6;
+            }
+            else if (cursorPosition.x < quadraticIndex_left8.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 7;
+            }
+            else if (cursorPosition.x < quadraticIndex_left9.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 8;
+            }
+            else if (cursorPosition.x < quadraticIndex_left10.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 9;
+            }
         }
         else if (cursorPosition.y > depth4)
         {
             selectGrid.y = 2;
+            if (cursorPosition.x < quadraticIndex_left0.BoundaryCalculation(cursorPosition))
+            {
+                //範囲外
+                selectGrid.x = -1;
+                selectGrid.y = -1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left1.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 0;
+            }
+            else if (cursorPosition.x < quadraticIndex_left2.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left3.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 2;
+            }
+            else if (cursorPosition.x < quadraticIndex_left4.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 3;
+            }
+            else if (cursorPosition.x < quadraticIndex_left5.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 4;
+            }
+            else if (cursorPosition.x < quadraticIndex_left6.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 5;
+            }
+            else if (cursorPosition.x < quadraticIndex_left7.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 6;
+            }
+            else if (cursorPosition.x < quadraticIndex_left8.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 7;
+            }
+            else if (cursorPosition.x < quadraticIndex_left9.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 8;
+            }
+            else if (cursorPosition.x < quadraticIndex_left10.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 9;
+            }
         }
         else if (cursorPosition.y > depth5)
         {
             selectGrid.y = 1;
+            if (cursorPosition.x < quadraticIndex_left0.BoundaryCalculation(cursorPosition))
+            {
+                //範囲外
+                selectGrid.x = -1;
+                selectGrid.y = -1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left1.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 0;
+            }
+            else if (cursorPosition.x < quadraticIndex_left2.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left3.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 2;
+            }
+            else if (cursorPosition.x < quadraticIndex_left4.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 3;
+            }
+            else if (cursorPosition.x < quadraticIndex_left5.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 4;
+            }
+            else if (cursorPosition.x < quadraticIndex_left6.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 5;
+            }
+            else if (cursorPosition.x < quadraticIndex_left7.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 6;
+            }
+            else if (cursorPosition.x < quadraticIndex_left8.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 7;
+            }
+            else if (cursorPosition.x < quadraticIndex_left9.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 8;
+            }
+            else if (cursorPosition.x < quadraticIndex_left10.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 9;
+            }
         }
         else if (cursorPosition.y > depth6)
         {
             selectGrid.y = 0;
+            if (cursorPosition.x < quadraticIndex_left0.BoundaryCalculation(cursorPosition))
+            {
+                //範囲外
+                selectGrid.x = -1;
+                selectGrid.y = -1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left1.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 0;
+            }
+            else if (cursorPosition.x < quadraticIndex_left2.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 1;
+            }
+            else if (cursorPosition.x < quadraticIndex_left3.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 2;
+            }
+            else if (cursorPosition.x < quadraticIndex_left4.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 3;
+            }
+            else if (cursorPosition.x < quadraticIndex_left5.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 4;
+            }
+            else if (cursorPosition.x < quadraticIndex_left6.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 5;
+            }
+            else if (cursorPosition.x < quadraticIndex_left7.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 6;
+            }
+            else if (cursorPosition.x < quadraticIndex_left8.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 7;
+            }
+            else if (cursorPosition.x < quadraticIndex_left9.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 8;
+            }
+            else if (cursorPosition.x < quadraticIndex_left10.BoundaryCalculation(cursorPosition))
+            {
+                selectGrid.x = 9;
+            }
         }
+        Debug.Log(selectGrid);
+        Debug.Log(cursorPosition);
+    }
+}
+
+public class QuadraticIndex
+{
+    public QuadraticIndex(float r,float d)
+    {
+        ratio = r;
+        difference = d;
+    }
+
+    public float ratio;
+    public float difference;
+
+    public float BoundaryCalculation(Vector2 value)
+    {
+        float boundaryPoint;
+        boundaryPoint = value.y * ratio + difference;
+        return boundaryPoint;
     }
 }
